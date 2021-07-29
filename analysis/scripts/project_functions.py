4324005817d83cf60198719614aa3ea4cd72db0f
kaminskg <gkamin6@gmail.com>
2021-07-29 07:23:46 -0700

    Remove unused function and edit formating of my notebook

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -4,16 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def convert(episodeLength):
-    episodeLength = episodeLength.split()
-    if episodeLength[1] == 'hr.':
-        return (int(episodeLength[0]) * 60) + int(episodeLength[2]) 
-    elif episodeLength[1] == 'min.':
-        return int(episodeLength[0])
-    else:
-        return 0
 
-    
 def returnList(df, column):
     return df[column].tolist()
 
@@ -60,9 +51,8 @@ def load_and_process(result):
         df1
         .drop(columns = ['MAL_ID', 'Producers', 'Licensors', 'English name', 'Japanese name', 'Score-10', 'Score-9', 'Score-8', 'Score-7', 'Score-6', 'Score-5', 'Score-4', 'Score-3', 'Score-2', 'Score-1'])
         .dropna(axis='rows')
-        .rename(columns={"Duration": "Duration (Minutes)"})
         .reset_index(drop = True)
-         .astype({"Episodes": int, "Ranked": float, "Score": float})  
+        .astype({"Episodes": int, "Ranked": float, "Score": float})  
     )
     
     #   df2['Date'] = pd.to_datetime(df2['Date'], format='%d/%m/%Y')
